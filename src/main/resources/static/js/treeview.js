6d2970d49253c8222aebecc42725036debc97f5a
seongho <skk7541@daou.co.kr>
2023-12-29 11:02:15 +0900

    husky 적용

---
+++ src/main/resources/static/js/treeview.js
@@ -0,0 +1,83 @@
+function drawTreeView(data) {
+  $("#tree-view").bstreeview({
+    data,
+  });
+}
+
+function findNodesById(nodes, targetId) {
+  for (let i = 0; i < nodes.length; i++) {
+    const currentNode = nodes[i];
+
+    if (currentNode.id === targetId) {
+      // 특정 id 값을 가진 노드를 찾으면 해당 노드의 nodes 값을 반환
+      return currentNode.nodes;
+    }
+    if (currentNode.nodes && currentNode.nodes.length > 0) {
+      // 현재 노드에 하위 노드가 있다면 재귀적으로 검색
+      const nestedNodes = findNodesById(currentNode.nodes, targetId);
+      if (nestedNodes) {
+        return nestedNodes; // 하위 노드에서 찾았으면 반환
+      }
+    }
+  }
+
+  // 해당 id를 찾지 못했을 경우
+  return null;
+}
+
+function loadUserData(targetId) {
+  $.ajax({
+    url: `user-data/${targetId}`,
+    type: "GET",
+    dataType: "json",
+  })
+    .done((data) => {
+      // table 비우기
+      $("#dataTable tbody").empty();
+
+      // table 채우기
+      for (let i = 0; i < data.length; i++) {
+        const user = data[i];
+        $("#dataTable tbody").append(
+          "<tr>" +
+            '<td><input type="checkbox"></td>' +
+            `<td>${user.organizationId}</td>` +
+            `<td>${user.username}</td>` +
+            `<td>${user.email}</td>` +
+            `<td>${user.createdAt}</td>` +
+            "</tr>",
+        );
+      }
+    })
+    .fail(() => {
+      alert("User Data Error!");
+    });
+}
+
+function addClickEvent(data) {
+  $(".list-group-item").on("click", function () {
+    const nodes = findNodesById(data, this.id);
+    if (nodes === null) {
+      loadUserData(this.id);
+    }
+  });
+}
+
+function initializeTreeView() {
+  $.ajax({
+    url: "tree-data",
+    type: "GET",
+    dataType: "json",
+  })
+    .done((data) => {
+      drawTreeView(data);
+      addClickEvent(data);
+    })
+    .fail(() => {
+      alert("Tree Data Error!");
+    });
+}
+
+$(document).ready(() => {
+  initializeTreeView();
+});
